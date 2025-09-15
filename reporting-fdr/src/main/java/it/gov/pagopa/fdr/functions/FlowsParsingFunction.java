73aa96770e6db9afabf38271864dc1a4680def44
gioelemella <128155546+gioelemella@users.noreply.github.com>
2025-09-15 09:18:36 +0200

    [PIDM-934] upgrade to version [4.0.0, 5.0.0)

--- reporting-fdr/src/main/java/it/gov/pagopa/fdr/functions/FlowsParsingFunction.java
+++ reporting-fdr/src/main/java/it/gov/pagopa/fdr/functions/FlowsParsingFunction.java
@@ -1,86 +1,95 @@
 package it.gov.pagopa.fdr.functions;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.BlobTrigger;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import it.gov.pagopa.fdr.service.FlowXmlParser;
 import it.gov.pagopa.fdr.service.OptionsService;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import javax.xml.bind.DatatypeConverter;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-/**
- * Azure Functions with Azure Blob trigger.
- */
+/** Azure Functions with Azure Blob trigger. */
+@Slf4j
 public class FlowsParsingFunction {
-    private final String storageConnectionString = System.getenv("FLOW_SA_CONNECTION_STRING");
-    private final String containerBlobOut = System.getenv("OUTPUT_BLOB");
-    private final String containerBlobIn = System.getenv("FLOWS_XML_BLOB");
 
-    /**
-     * This function will be invoked when a new or updated blob is detected at the
-     * specified path. The blob contents are provided as input to this function.
-     */
-    @FunctionName("FlowsParsingFunction")
-    public void run(
-            @BlobTrigger(name = "BlobXmlTrigger", path = "%FLOWS_XML_BLOB%/{name}", dataType = "binary", connection = "FLOW_SA_CONNECTION_STRING") byte[] content,
-            @BindingName("name") String name, final ExecutionContext context) {
+  private final OptionsService optionsService;
 
-        Logger logger = context.getLogger();
+  public FlowsParsingFunction(OptionsService optionsService) {
+    this.optionsService = optionsService;
+  }
 
-        logger.log(Level.INFO, () -> "Blob Trigger function executed at: " + LocalDateTime.now() + " for blob " + name);
+  public FlowsParsingFunction() {
+    this.optionsService = new OptionsService();
+  }
 
+  /**
+   * This function will be invoked when a new or updated blob is detected at the specified path. The
+   * blob contents are provided as input to this function.
+   */
+  @FunctionName("FlowsParsingFunction")
+  public void run(
+      @BlobTrigger(
+              name = "BlobXmlTrigger",
+              path = "%FLOWS_XML_BLOB%/{name}",
+              dataType = "binary",
+              connection = "FLOW_SA_CONNECTION_STRING")
+          byte[] content,
+      @BindingName("name") String name,
+      final ExecutionContext context) {
+    log.info("Blob Trigger function executed at: {} for blob {}", LocalDateTime.now(), name);
 
-        String convertedStr = new String(content, StandardCharsets.UTF_8);
+    String convertedStr = new String(content, StandardCharsets.UTF_8);
+    String converted = new String(DatatypeConverter.parseBase64Binary(convertedStr));
 
-        String converted= new String(DatatypeConverter.parseBase64Binary(convertedStr));
+    try {
+      SAXParserFactory factory = SAXParserFactory.newInstance();
+      // to be compliant, completely disable DOCTYPE declaration:
+      factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
 
-        try {
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            // to be compliant, completely disable DOCTYPE declaration:
-            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+      SAXParser saxParser = factory.newSAXParser();
 
-            SAXParser saxParser = factory.newSAXParser();
+      FlowXmlParser handler = new FlowXmlParser();
+      saxParser.parse(new InputSource(new StringReader(converted)), handler);
 
-            FlowXmlParser handler = new FlowXmlParser();
-            saxParser.parse(new InputSource(new StringReader(converted)), handler);
+      // identificativoPSP--identificativoIntermediarioPSP--identificativoCanale--identificativoDominio--identificativoFlusso--dataOraFlusso.xml
+      // AGID_01--97735020584--97735020584_03--77777777777--2022-01-24GID_01-S003035679--2022-01-24T00:30:49.xml
+      String[] flowInfo = name.split("--");
+      String identificativoPSP = flowInfo[0];
+      String identificativoIntermediarioPSP = flowInfo[1];
+      String identificativoCanale = flowInfo[2];
+      String identificativoDominio = flowInfo[3];
+      String identificativoFlusso = flowInfo[4];
+      String dataOraFlusso =
+          flowInfo[5].substring(0, flowInfo[5].length() - 4); // remove extension file
+      log.info(
+          "Processing flow PSP {} flow {} with date {}",
+          identificativoPSP,
+          identificativoFlusso,
+          dataOraFlusso);
+      this.optionsService.optionsProcessing(
+          handler.getIdentificativoUnivocoRegolamento(),
+          handler.getDataRegolamento(),
+          handler.getOptions(),
+          identificativoPSP,
+          identificativoIntermediarioPSP,
+          identificativoCanale,
+          identificativoDominio,
+          identificativoFlusso,
+          dataOraFlusso);
 
-            OptionsService optionsService = this.getOptionsServiceInstance(logger);
-
-            // identificativoPSP--identificativoIntermediarioPSP--identificativoCanale--identificativoDominio--identificativoFlusso--dataOraFlusso.xml
-            // AGID_01--97735020584--97735020584_03--77777777777--2022-01-24GID_01-S003035679--2022-01-24T00:30:49.xml
-            String[] flowInfo = name.split("--");
-            String identificativoPSP = flowInfo[0];
-            String identificativoIntermediarioPSP = flowInfo[1];
-            String identificativoCanale = flowInfo[2];
-            String identificativoDominio = flowInfo[3];
-            String identificativoFlusso = flowInfo[4];
-            String dataOraFlusso = flowInfo[5].substring(0,flowInfo[5].length()-4); // remove extension file
-            logger.log(Level.INFO, () -> "Processing flow PSP " + identificativoPSP + " flow " + identificativoFlusso + " with date " + dataOraFlusso);
-            optionsService.optionsProcessing(handler.getIdentificativoUnivocoRegolamento(), handler.getDataRegolamento(), handler.getOptions(), identificativoPSP, identificativoIntermediarioPSP, identificativoCanale, identificativoDominio, identificativoFlusso, dataOraFlusso);
-
-            optionsService.shift2OutFile(name, converted);
-
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            logger.log(Level.INFO, () -> "Processing flow exception: " + e.getMessage());
-        }
+      this.optionsService.shift2OutFile(name, converted);
+    } catch (ParserConfigurationException | SAXException | IOException e) {
+      log.error("Processing flow exception", e);
     }
-
-    public OptionsService getOptionsServiceInstance(Logger logger) {
-
-        return new OptionsService(this.storageConnectionString, logger, this.containerBlobOut, this.containerBlobIn);
-    }
-
+  }
 }
